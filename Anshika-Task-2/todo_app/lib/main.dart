084248c395f9fdd5538b994958513931dfe5df16
anshikasisodia4 <anshikasisodiya81@gmail.com>
2026-09-06 20:17:39 +0530

    Add Todo app

---
+++ Anshika-Task-2/todo_app/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'pages/splash_page.dart';
+
+void main() {
+  runApp(const TodoApp());
+}
+
+class TodoApp extends StatelessWidget {
+  const TodoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ToDo App',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 96, 117, 148),
+        ),
+      ),
+      home: const SplashPage(),
+    );
+  }
+}
---
+++ Anshika-Task-2/todo_app/lib/pages/home_page.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import '../models/task.dart';
+import '../widgets/task_card.dart';
+import 'add_task_page.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<Task> tasks = [];
+
+  // Add task
+  void addTask(String title, String description) {
+    setState(() {
+      tasks.add(Task(title: title, description: description));
+    });
+  }
+
+  // Delete task
+  void deleteTask(int index) {
+    setState(() {
+      tasks.removeAt(index);
+    });
+  }
+
+  // Complete or pending
+  void toggleTask(int index) {
+    setState(() {
+      tasks[index].isCompleted = !tasks[index].isCompleted;
+    });
+  }
+
+  // Edit task
+  void editTask(int index) async {
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => AddTaskPage(task: tasks[index])),
+    );
+
+    if (result != null) {
+      setState(() {
+        tasks[index].title = result['title'];
+        tasks[index].description = result['description'];
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'My Tasks',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+
+      // View tasks
+      body: tasks.isEmpty
+          ? const Center(
+              child: Text(
+                'No tasks yet\nAdd a task to get started!',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 18, color: Colors.grey),
+              ),
+            )
+          : ListView.builder(
+              padding: const EdgeInsets.all(16),
+              itemCount: tasks.length,
+              itemBuilder: (context, index) {
+                return TaskCard(
+                  task: tasks[index],
+                  onDelete: () => deleteTask(index),
+                  onToggle: () => toggleTask(index),
+                  onEdit: () => editTask(index),
+                );
+              },
+            ),
+
+      // Add task
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final result = await Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const AddTaskPage()),
+          );
+
+          if (result != null) {
+            addTask(result['title'], result['description']);
+          }
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
