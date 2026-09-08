ee541f0452b476572d46e72b5eac5293d77cac8a
anshikasisodia4 <anshikasisodiya81@gmail.com>
2026-09-08 15:30:24 +0530

    change theme

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,31 +1,31 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-static ThemeData lightTheme = ThemeData(
-primarySwatch: Colors.deepPurple,
+  static ThemeData lightTheme = ThemeData(
+    scaffoldBackgroundColor: const Color(0xFFFFF5F7),
 
-
-scaffoldBackgroundColor: Colors.white,
-
-appBarTheme: const AppBarTheme(
-  backgroundColor: Colors.deepPurple,
-  foregroundColor: Colors.white,
-),
-
-floatingActionButtonTheme: const FloatingActionButtonThemeData(
-  backgroundColor: Colors.deepPurple,
-  foregroundColor: Colors.white,
-),
-
-inputDecorationTheme: const InputDecorationTheme(
-  focusedBorder: OutlineInputBorder(
-    borderSide: BorderSide(
-      color: Colors.deepPurple,
-      width: 2,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Color(0xFFF4B6C2),
+      foregroundColor: Color(0xFF4A3035),
     ),
-  ),
-),
 
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: Color(0xFFE58FA0),
+      foregroundColor: Colors.white,
+    ),
 
-);
+    inputDecorationTheme: const InputDecorationTheme(
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: Color(0xFFE58FA0),
+          width: 2,
+        ),
+      ),
+    ),
+
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: const Color(0xFFE58FA0),
+      brightness: Brightness.light,
+    ),
+  );
 }
