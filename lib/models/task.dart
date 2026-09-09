a24054fe9adf72ff937749c16d1631f1e2e893ba
anshikasisodia4 <anshikasisodiya81@gmail.com>
2026-09-09 07:21:52 +0530

    added id

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -4,8 +4,26 @@ class Task {
   bool isCompleted;
 
   Task({
+       this.id,
     required this.title,
     this.description = '',
     this.isCompleted = false,
   });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'title': title,
+      'description': description,
+      'isCompleted': isCompleted,
+    };
+  }
+
+  factory Task.fromMap(String id, Map<String, dynamic> map) {
+    return Task(
+      id: id,
+      title: map['title'] ?? '',
+      description: map['description'] ?? '',
+      isCompleted: map['isCompleted'] ?? false,
+    );
+  }
 }
